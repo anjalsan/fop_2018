becfd2786e5fba5e088e275a739d63ff0f64f1f7
anjalsaneen <anjalsaneen123@gmail.com>
2018-09-15 13:36:39 +0530

    Kotlin and mvRx setep

---
+++ app/src/main/java/com/fpl/fop/fop/mvrx/TallyDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.fpl.fop.fop.mvrx
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fpl.fop.fop.R
+
+class TallyDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tally_details)
+    }
+}
